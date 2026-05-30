036255d8f9d20b9a20554c3120274c0f328736f7
Study Developer <developer@study.com>
2026-05-30 12:45:17 +0530

    feat(android): integrate native Start.io (StartApp) SDK in app build.gradle and MainActivity

--- frontend/android/app/src/main/java/com/study/material/platform/MainActivity.java
+++ frontend/android/app/src/main/java/com/study/material/platform/MainActivity.java
@@ -1,5 +1,18 @@
 package com.study.material.platform;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.startapp.sdk.adsbase.StartAppSDK;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Start.io (StartApp) SDK with App ID: 204789628
+        // Set Return Ads disabled for optimal student user experience
+        StartAppSDK.initParams(this, "204789628")
+            .setReturnAdsEnabled(false)
+            .init();
+    }
+}
